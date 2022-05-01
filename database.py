3d35ab7c6b84a4d36562fae2b06c85019c54bba5
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-04-30 23:48:14 -0500

    Tables simplified, insert and select functions added

--- database.py
+++ database.py
@@ -6,30 +6,58 @@ def start():
 
     cur.execute(
         '''CREATE TABLE IF NOT EXISTS users 
-        (id INTEGER PRIMARY KEY,
-        name TEXT,
-        email TEXT,
-        password TEXT)'''
+        (username TEXT)'''
     )
 
     cur.execute(
         '''CREATE TABLE IF NOT EXISTS courses
-        (id INTEGER PRIMARY KEY,
-        name TEXT)'''
+        (name TEXT)'''
     )
 
 
     cur.execute('''CREATE TABLE IF NOT EXISTS tasks
-        (id INTEGER PRIMARY KEY,
-        name TEXT,
+        (name TEXT,
         course_id INTEGER,
         due_date DATE,
         priority INTEGER,
         user_id INTEGER,
         FOREIGN KEY(course_id) REFERENCES courses(id),
-        FOREIGN KEY(user_id) REFERENCES users(id))''')
-
+        FOREIGN KEY(user_id) REFERENCES users(id))'''
+    )
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS settings
+        (name TEXT,
+        value TEXT)'''
+    )
+    
+    con.commit()
     return con, cur
+
+def lastrowid(cur):
+    id = cur.execute("SELECT last_insert_rowid()")
+    return id.fetchone()[0]
+    
+
+# @param data: a list of tuples
+def insert(con, cur, table, column_names, data):
+    query = "INSERT INTO {} ".format(table)
+    
+    if column_names is not None:
+        query += "({}) ".format(",".join(column_names))
+
+    query += "VALUES ({})".format(",".join(["?"] * len(data[0])))
+    # print("query: {}".format(query))
+    # print("data: {}".format(data))
+    cur.executemany(query, data)
+    con.commit()
+
+
+def select(cur, table, column_names, where_clause, where_data):
+    query = "SELECT {} FROM {} ".format(",".join(column_names), table)
+    if where_clause is not None:
+        query += "WHERE {} ".format(where_clause)
+    cur.execute(query, where_data)
+    return cur.fetchall()
 
 def terminate(con):
     con.close()
