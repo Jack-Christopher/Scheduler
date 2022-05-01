783c97514a5ac7b0852ec85dbb583416f3c836b2
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-04-30 23:49:06 -0500

    add_user function and main body added

--- main.py
+++ main.py
@@ -2,7 +2,31 @@ import database as db
 
 class Schedule:
     def __init__(self):
-        con, cur = db.start()
+        self.con, self.cur = db.start()
+    
+    def add_user(self, username):
+        db.insert(self.con, self.cur, "users", ["username"], [(username,)])
+        self.con.commit()
+        return db.lastrowid(self.cur)
+    
+    def set_current_user(self, user_id):
+        db.insert(self.con, self.cur, "settings", ["name", "value"], [("current_user", str(user_id))])
+        self.con.commit()
+
         
     def __del__(self):
-        db.terminate(con)
+        db.terminate(self.con)
+    
+
+if __name__ == "__main__":
+    S = Schedule()
+    current_user = db.select(S.cur, "settings", ["value"], "name=:name", {"name": "current_user"})
+    print(current_user)
+    if current_user == []:
+        username = input("Enter a username: ")
+        user_id = S.add_user(username)
+        S.set_current_user(user_id)
+        current_user = user_id
+    
+    
+    
