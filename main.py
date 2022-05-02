436fc9111f3e57da2332931348c643d8cb741445
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-05-02 11:46:35 -0500

    Create get_current_user function

--- main.py
+++ main.py
@@ -12,22 +12,25 @@ class Schedule:
     def set_current_user(self, user_id):
         db.insert(self.con, self.cur, "settings", ["name", "value"], [("current_user", str(user_id))])
         self.con.commit()
+    
+    def get_current_user(self):
+        current_user = db.select(S.cur, "settings", ["value"], "name=?", ("current_user", ))
+        if current_user == []:
+            username = input("Enter a username: ")
+            user_id = S.add_user(username)
+            self.set_current_user(user_id)
+            current_user = user_id
+        else:
+            current_user = int(current_user[0][0])
+        username = db.select(S.cur, "users", ["username"], "rowid=?", (current_user,))[0][0]
 
-        
+        return username
+
     def __del__(self):
         db.terminate(self.con)
     
 
 if __name__ == "__main__":
     S = Schedule()
-    current_user = db.select(S.cur, "settings", ["value"], "name=?", ("current_user", ))
-    # print(current_user)
-    if current_user == []:
-        username = input("Enter a username: ")
-        user_id = S.add_user(username)
-        S.set_current_user(user_id)
-        current_user = user_id
-    else:
-        current_user = int(current_user[0][0])
-    username = db.select(S.cur, "users", ["username"], "rowid=?", (current_user,))[0][0]
+    username = S.get_current_user()
     print("Welcome, {}!".format(username))
